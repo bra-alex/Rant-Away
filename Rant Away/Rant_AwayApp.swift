9905a7c28939ee371cc4687fc44d7f1198e6153b
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-11-25 07:22:46 +0000

    Initial Commit

---
+++ Rant Away/Rant_AwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rant_AwayApp.swift
+//  Rant Away
+//
+//  Created by Don Bouncy on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Rant_AwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
